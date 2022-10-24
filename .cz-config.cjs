475cb7f8b5a57a787d06e72544afd635945bb96b
christol <christol.yu@outlook.com>
2022-10-24 19:09:50 -0400

    [🦄 refactor] commitizen configs

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -6,7 +6,7 @@ module.exports = {
         { value: '🐞 fix', name: '🐞fix:      Bug fix' },
         { value: '🚧 working', name: '🚧working: Ongoing work' },
         { value: '📝 docs', name: '📝docs:     Documentation only changes' },
-        { value: '🔧 chore', name: '🔧chore:    Changes to the libraries and auxiliary tools or build process' },
+        { value: '🔧 chore', name: '🔧chore:    Changes to the libraries/tools/build process' },
         { value: '📐 format', name: '📐format:   Code formatting(without changes to functionaliteis)' },
         { value: '🦄 refactor', name: '🦄refactor: Changes that neither fix bugs nor add features' },
         { value: '🚀 perf', name: '🚀perf:     Performance improvements' },
@@ -14,9 +14,12 @@ module.exports = {
         { value: '⏪ revert', name: '⏪revert:   Rollback' },
         { value: '🔨 build', name: '🔨build:    Build up' }
     ],
+
+    scopes: [{ name: 'landing' }, { name: 'shopping page' }, { name: 'check out' }, { name: 'payment' }],
     // messaging steps
     messages: {
         type: 'Select the type of your commit:',
+        scope: '\nDenote the SCOPE of this change (optional):',
         customScope: 'Input the scope of changes(optional):',
         subject: 'Briefly describe the changes(required):',
         body: 'Detailed desciptions(optional):',
@@ -24,5 +27,9 @@ module.exports = {
         confirmCommit: 'Commit with above information?(y/n/e/h)'
     },
     // length limit of subject content
-    subjectLimit: 72
+    subjectLimit: 100,
+    allowCustomScopes: true,
+    typePrefix: '[',
+    typeSuffix: ']',
+    subjectSeparator: ' '
 };
