cec6ac8148e5560668995e2f942fc9a9ffd5e04a
Zahit Odabas <odabaszahit@gmail.com>
2022-11-02 11:37:35 -0400

    [🚧 working](landing) added some style adjustments to Welcome component

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -4,11 +4,13 @@ import doctor from '../../assets/doctor.png';
 const Welcome = () => {
     return (
         <div className="welcome">
-            Get your medications <br />
-            delivered to you <br />
-            <a className="link" href="">
-                See all of our products
-            </a>
+            <div>
+                Get your medication <br />
+                delivered to you <br />
+                <a className="link" href="">
+                    See all of our products {'\u2192'}
+                </a>
+            </div>
             <div>
                 <img src={doctor} className="happy-doctor"></img>
             </div>
