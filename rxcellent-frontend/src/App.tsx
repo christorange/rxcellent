6ba1a61c027063f55646a79b1d86e644f471f210
Christol Yu <christol.yu@outlook.com>
2022-11-11 17:22:22 -0500

    🔧 chore: lint stage config

--- rxcellent-frontend/src/App.tsx
+++ rxcellent-frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
-
-import React, { useState } from 'react';
-
-import logo from './assets/logo.png';
-import Box from '@mui/material/Box';
 import Banner from './pages/shopping/components/banner';
+
+var num: number = '123123';
 
 function App() {
     return (
