23a7d8ec83d200e1b238f0c0d4800c3b559afe72
Christol Yu <christol.yu@outlook.com>
2022-11-11 16:03:32 -0500

    🔧 chore: package and cz configs

--- .cz-config.js
+++ .cz-config.js
@@ -4,11 +4,12 @@ module.exports = {
     { value: "🎉 init", name: "🎉init:     Project initialization" },
     { value: "✨ feat", name: "✨feat:     New feature" },
     { value: "🐞 fix", name: "🐞fix:      Bug fix" },
+    { value: "🎨 style", name: "🎨style:    Styling changes" },
     { value: "🚧 working", name: "🚧working: Ongoing work" },
     { value: "📝 docs", name: "📝docs:     Documentation only changes" },
     {
       value: "🔧 chore",
-      name: "🔧chore:    Changes to the libraries and auxiliary tools or build process",
+      name: "🔧chore:    Changes to the libraries/tools/build process",
     },
     {
       value: "📐 format",
@@ -23,9 +24,17 @@ module.exports = {
     { value: "⏪ revert", name: "⏪revert:   Rollback" },
     { value: "🔨 build", name: "🔨build:    Build up" },
   ],
+
+  scopes: [
+    { name: "landing" },
+    { name: "shop" },
+    { name: "check out" },
+    { name: "payment" },
+  ],
   // messaging steps
   messages: {
     type: "Select the type of your commit:",
+    scope: "\nDenote the SCOPE of this change (optional):",
     customScope: "Input the scope of changes(optional):",
     subject: "Briefly describe the changes(required):",
     body: "Detailed desciptions(optional):",
@@ -33,5 +42,6 @@ module.exports = {
     confirmCommit: "Commit with above information?(y/n/e/h)",
   },
   // length limit of subject content
-  subjectLimit: 72,
+  subjectLimit: 100,
+  allowCustomScopes: true,
 };
